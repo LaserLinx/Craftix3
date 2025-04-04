0109cbf8effafc88dbf35121fa981841383ccb37
LaserLinx <135711444+LaserLinx@users.noreply.github.com>
2025-04-04 21:11:39 +0200

    Add files via upload

--- core/ui/settings.py
+++ core/ui/settings.py
@@ -5,6 +5,7 @@ from tkinter import filedialog
 from core.ui import colorschem
 import customtkinter as ctk
 import os
+
 from core import DataAPI
 config = {
 	"render3d": False,
@@ -23,7 +24,7 @@ except:
 	config = {
 	"render3d": False,
 	"BorderRender": False,
-	"ExportPath": "./exported_data/",
+	"ExportPath": "./",
 	"mode": "light",
 	"them": "default"
 	}
@@ -49,15 +50,18 @@ def open(screen):
 		update_settings()
 		DataAPI.update_them()	
 	def chose_export_path():
-			path = filedialog.askdirectory()
+		temp_path = config.get("ExportPath")
+		path = filedialog.askdirectory()
+		if not path == "":
 			print(path)
-			export_path_entry.delete(0,tk.END)
-			export_path_entry.insert(0,path)
 			config["ExportPath"] = str(path)
 			update_settings()
-	def update_export_path(value=None):
-		config["ExportPath"] = str(export_path_entry.get())
-		update_settings()
+		else:
+			path = temp_path
+			config["ExportPath"] = str(path)
+			update_settings()
+		export_path_entry.configure(text=config.get("ExportPath"))
+		export_path_entry._text=config.get("ExportPath")
 
 	them_mode_var = ctk.StringVar(screen)
 	them_mode_ws = ctk.CTkSwitch(screen, text="Light/Dark Mode", command=lambda:update_them(them_mode_var.get()),
@@ -76,12 +80,12 @@ def open(screen):
 	them_selector.pack(side="top",pady=10)
 	them_selector.set(config.get("them"))
 
-	export_path_entry = ctk.CTkEntry(screen,width=500)
+	export_path_entry = ctk.CTkLabel(screen,width=500,text=config.get("ExportPath"))
 	export_path_entry.pack(pady=10)
-	export_path_entry.delete(0,tk.END)
-	export_path_entry.insert(0,config.get("ExportPath"))
+
 	export_path_set_button = ctk.CTkButton(screen,command=chose_export_path,text="Chose Floder")
 	export_path_set_button.pack(pady=10)
-	export_path_entry.bind("<KeyRelease>",update_export_path)
+	
+	
 
 
